195a2726c730178f6b03277bc461b32884c64685
Andriy Khimich <andrykhimich@gmail.com.noreply.github.com>
2020-12-07 18:05:50 +0200

    home page

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -5,64 +5,36 @@ const header = $(".header");
 const logoImg = $(".logo img");
 
 function setInnerHeader() {
-	logoImg.attr("src", logoBlackUrl);
-	header.addClass("header_inner");
+  logoImg.attr("src", logoBlackUrl);
+  header.addClass("header_inner");
 }
 
 function setHomeHeader() {
-	logoImg.attr("src", logoMainUrl);
-	header.removeClass("header_inner");
+  logoImg.attr("src", logoMainUrl);
+  header.removeClass("header_inner");
 }
-// function showOnScroll(scrollValue) {
-//   $('.js-scroll').each(function () {
-//     let elem = $(this);
-//     let sectionPos = elem.offset().top;
-//     let windowPos = $(window).scrollTop() + $(window).height() / 1.2;
-//     if (sectionPos < windowPos) {
-//       elem.removeClass('js-fadeIn js-slideLeft js-slideRight js-slideTop');
-//     }
-//   });
 
-//   $('.js-active').each(function () {
-//     let item = $(this);
-//     let sectionPos = item.offset().top;
-//     let windowPos = $(window).scrollTop() + $(window).height() / 2.8;
-//     if (sectionPos < windowPos) {
-//       item.addClass('active');
-//     } else {
-//       item.removeClass('active');
-//     }
-//   });
-// }
-
-// function openMenu() {
-//   humburger.addClass('open');
-//   headerMenu.addClass('open');
-// }
-
-// function closeMenu() {
-//   humburger.removeClass('open');
-//   headerMenu.removeClass('open');
-// }
 
 function showContent() {
-	$(".main-wrapper").removeClass("js-fadeIn");
+  $(".main-wrapper").removeClass("js-fadeIn");
 }
 
 $(document).ready(function () {
-	// if ($('.inner-page').length > 0) {
-	//   setInnerHeader();
-	// } else {
-	//   setHomeHeader();
-	// }
-	showContent();
+  $('.custom-select').niceSelect();
+  // $('.selectpicker').selectpicker();
+  // if ($('.inner-page').length > 0) {
+  //   setInnerHeader();
+  // } else {
+  //   setHomeHeader();
+  // }
+  showContent();
 
-	// humburger.click(function () {
-	//   if ($(this).hasClass('open')) {
-	//     closeMenu();
-	//   } else {
-	//     openMenu();
-	//   }
+  // humburger.click(function () {
+  //   if ($(this).hasClass('open')) {
+  //     closeMenu();
+  //   } else {
+  //     openMenu();
+  //   }
 });
 
 // slow scroll to id
@@ -90,18 +62,22 @@ $(document).ready(function () {
 //     }
 //   });
 
-// $('.home-slider').slick({
-//   slidesToShow: 1,
-//   slidesToScroll: 1,
-//   autoplay: true,
-//   dots: true,
-//   arrows: false,
-//   infinite: true,
-//   fade: true,
-//   speed: 1000,
-//   cssEase: 'linear',
-//   autoplaySpeed: 10000
-// });
+$('.testimonials-slider').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  arrows: true,
+  infinite: false,
+  prevArrow: $('.slider-btn_prev'),
+  nextArrow: $('.slider-btn_next'),
+  responsive: [{
+    breakpoint: 768,
+    settings: {
+      infinite: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    }
+  }]
+});
 // $('.testimonials-slider__wrapper').slick({
 //   slidesToShow: 1,
 //   slidesToScroll: 1,
@@ -119,20 +95,3 @@ $(document).ready(function () {
 // });
 // });
 svg4everybody();
-
-function testWebP(callback) {
-	var webP = new Image();
-	webP.onload = webP.onerror = function () {
-		callback(webP.height == 2);
-	};
-	webP.src =
-		"data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-}
-
-testWebP(function (support) {
-	if (support == true) {
-		document.querySelector("body").classList.add("webp");
-	} else {
-		document.querySelector("body").classList.add("no-webp");
-	}
-});
